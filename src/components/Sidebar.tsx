80af98635e6dece1afcd6c6f128c71a35e4bec45
Nand Augustynen <nandaugustynen@12:28:20:60:71:df.home>
2026-09-16 15:55:25 +0200

    Added delay sidebar expanding

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -46,7 +46,7 @@ export function Sidebar({
 
   function handleMouseEnter() {
     cancelExpansion()
-    expandTimer.current = setTimeout(() => setExpanded(true), 350)
+    expandTimer.current = setTimeout(() => setExpanded(true), 600)
   }
 
   function handleMouseLeave() {
